4a0e937b831ce96a43bb46779a3513edc3350a6e
Abrshronaldo <abab61534@gmail.com>
2025-11-19 15:50:22 +0300

    z

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.getElementById("demo").style.color = "red";
 document.getElementById("myButton").onclick = function() {
     alert("Button clicked!");
 };
+// Get the canvas element
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+// Draw a line
+ctx.beginPath();       // start a new path
+ctx.moveTo(50, 50);    // starting point (x=50, y=50)
+ctx.lineTo(300, 150);  // ending point (x=300, y=150)
+ctx.stroke();          // actually draw the line
