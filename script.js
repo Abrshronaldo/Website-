bc3173e493efd58c0754f0d1ee0b5e4dbd09e090
Abrshronaldo <abab61534@gmail.com>
2025-11-28 18:29:11 +0300

    d

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ document.getElementById("myButton").onclick = function() {
     alert("Button clicked!");
 };
 
+function App() {
+  return <h1>Hello, React!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
