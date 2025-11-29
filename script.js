4f56e1804f6363d5d97819c63a4fd5bf00f1f1d8
Abrshronaldo <abab61534@gmail.com>
2025-11-29 14:24:01 +0300

    d

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ function App() {
   return <h1>Hello, React!</h1>;
 }
 
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+root.render(<Counter />);
