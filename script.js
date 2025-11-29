40fdc852c7d9db46bbffa7614683afcdf1a7b458
Abrshronaldo <abab61534@gmail.com>
2025-11-29 18:40:22 +0300

    d

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
 
 
-import React, { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <Counter />
+    </div>
+  );
 }
-
 
 function Counter() {
   const [count, setCount] = React.useState(0);
@@ -15,13 +20,11 @@ function Counter() {
     <div>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increase</button>
-
       <button onClick={() => setCount(count - 1)}>Decrease</button>
-    <Counter />
-
     </div>
   );
 }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
