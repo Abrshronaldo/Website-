16313be254f72728b04e3c30ca9ea3c08d4fce3b
Abrshronaldo <abab61534@gmail.com>
2025-11-29 21:25:43 +0300

    d

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function Counter() {
 
   return (
     <div>
-      <h1>Count: {count}</h1>
+      <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
