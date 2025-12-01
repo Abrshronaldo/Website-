c7c6a1f8c05e9ac22da539d6ed1068e8c899b04c
Abrshronaldo <abab61534@gmail.com>
2025-12-01 15:49:02 +0300

    d

--- script.js
+++ script.js
@@ -3,28 +3,7 @@
 
 
 
-const { useState } = React;
-function App() {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-      <Counter />
-    </div>
-  );
-}
 
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-
+function myCtrl($scope) {
+      $scope.name = "Ab"; // initial value
+    }
